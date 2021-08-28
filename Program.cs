c2a6b36d8213ac09e3e9ae1fa5b5a280a047eb84
Mueller <Stefan.Mueller@hitachi-automotive.eu>
2021-08-28 15:43:37 +0200

    Inital commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace npoi_tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
