939b0ace189e6fbe8127ece31f56e7f5a38ae15e
Stefan Müller <stefan.mueller@hitachi-automotive.eu>
2021-08-30 16:24:46 +0200

    log4net Logging enabled

--- Program.cs
+++ Program.cs
@@ -1,47 +1,24 @@
-﻿using System;
-using System.Data;
-using System.Data.SqlClient;
+﻿using log4net;
 
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
 namespace npoi_tests
 {
-    public class Program
+    public static class Program
     {
-        private readonly ILogger<Worker> _logger;
-
-        public Worker(ILogger<Worker> logger) =>
-            _logger = logger;
-
-        static void Main(string[] args)
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+        public static void Main(string[] args)
         {
+
+            log.Info("Entering application.");
+
             const string BudgetFileLocation = "D:\\src\\C#\\npoi-tests";
             const string DETemplateName = "Template FC DE.xltx";
             const string ENGTemplateName = "Template FC ENG.xltx";
             const string BudgetSheetName = "Data";
 
-            string connectionString = GetConnectionString();
-            ConnectToData(connectionString);
-        }
+            string connectionString = Sql.GetConnectionString();
+            Sql.ConnectToData(connectionString);
 
-        private static void ConnectToData(string connectionString)
-        {
-            using(SqlConnection connection = new SqlConnection(connectionString))
-            {
-                SqlDataAdapter adapter = new SqlDataAdapter();
-                System.Console.WriteLine("Try to open database: " + connectionString);
-                connection.Open();
-            }
-        }
-
-        private static string GetConnectionString() 
-        {
-            // TODO: get connection string from config file or database
-#if DEBUG
-            return "Data Source=(local); Database=SysproCompanyS;"
-                + "Integrated Security=SSPI";
-#else
-            return "Data Source=V-SX-SQL01; Database=SysproCompanyS;"
-                + "User Id=VBApplications;Password=password;";
-#endif
         }
     }
 }
